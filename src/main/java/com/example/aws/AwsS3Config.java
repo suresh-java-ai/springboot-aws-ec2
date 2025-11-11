94f9ea4b2ec484abcd9452015c5a06b98347e5ca
suresh <sajjasureshbabu65@gmail.com>
2025-11-11 15:51:59 +0530

    added safety credentials using parameter store

--- src/main/java/com/example/aws/AwsS3Config.java
+++ src/main/java/com/example/aws/AwsS3Config.java
@@ -1,33 +1,21 @@
 package com.example.aws;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
-    @Value("${aws.accessKey}")
-    private String accessKey;
 
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
+    private static final String AWS_REGION = "ap-south-1"; // ✅ you can also move this to application.properties
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
+                .region(Region.of(AWS_REGION))
+                .credentialsProvider(DefaultCredentialsProvider.create()) // ✅ automatically finds creds
                 .build();
     }
 }
